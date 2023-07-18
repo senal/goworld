d2b046bc31fa6639d9a4748fc7c9371fa2f034fa
Ranga Senal Fernando <ranga.fernando@gmail.com>
2023-07-18 22:13:00 +1000

    variables

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	// we use := when initializing for the first time
+	card := "Ace of Spades"
+
+	// re-assign a value to an existing variable
+	card = "Five of Diamonds"
+
+	fmt.Println(card)
+}
