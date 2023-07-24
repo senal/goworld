e257d988587b5358fbfe5c00c8038ea6e83fe959
Ranga Senal Fernando <ranga.fernando@gmail.com>
2023-07-24 22:42:23 +1000

    Add tests

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -15,4 +18,17 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d)-1] != "Eight of Spades" {
 		t.Errorf("Expected element is Ace of Clubs but found %v", d[len(d)-1])
 	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	fn := "_decktesting"
+	os.Remove(fn)
+	deck := newDeck()
+	deck.saveToFile(fn)
+	loadedDeck := newDeckFromFile(fn)
+	if len(loadedDeck) != 32 {
+		t.Errorf("Expected 32 cards and deck got %v", len(loadedDeck))
+	}
+	os.Remove(fn)
+
 }
