db8bf594becf918d593d072e415fefa59c50c29c
Ranga Senal Fernando <ranga.fernando@gmail.com>
2023-07-29 13:42:37 +1000

    Pointer shortcuts

--- structs/main.go
+++ structs/main.go
@@ -13,25 +13,35 @@ type person struct {
 	contact
 }
 
-func (p person) print() {
-	fmt.Printf("%+v", p)
-}
-
-func (pointerToPerson *person) updateFirstName(newFirstName string) {
-	pointerToPerson.firstName = newFirstName
-}
-
 func main() {
 	jim := person{
 		firstName: "Jim",
 		lastName:  "Action",
+		contact: contact{
+			email:   "j@j.com.au",
+			zipCode: 1234,
+		},
+	}
+
+	ranga := person{
+		firstName: "Ranga",
+		lastName:  "Fernando",
 		contact: contact{
 			email:   "r@r.com.au",
 			zipCode: 1234,
 		},
 	}
 
-	jimmPointer := &jim
-	jimmPointer.updateFirstName("Jimmy")
+	ranga.updateFirstName("Suranga")
+	//jim.updateFirstName("Jimmy")
 	jim.print()
+	ranga.print()
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
+
+func (pointerToPerson *person) updateFirstName(newFirstName string) {
+	(pointerToPerson).firstName = newFirstName
 }
