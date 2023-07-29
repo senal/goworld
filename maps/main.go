85fcfdd810f3d28ad662d48ac3b1687acf7d661b
Ranga Senal Fernando <ranga.fernando@gmail.com>
2023-07-29 17:14:36 +1000

    Iterating over maps

--- maps/main.go
+++ maps/main.go
@@ -5,19 +5,18 @@ import "fmt"
 func main() {
 	// ways to make an emtpy map
 	//1. var colors map[string]string
-	colors := make(map[string]string)
+	colors := map[string]string{
+		"red":   "this is  red",
+		"white": "this is white",
+		"green": "this is green",
+		"blue":  "this is blue",
+	}
+	printElements(colors)
 
-	// how to append elements to an empty map
-	colors["red"] = "this is red"
-	colors["white"] = "this is white"
+}
 
-	// colors := map[string]string{
-	// 	"red":   "this is red",
-	// 	"green": "this is green",
-	// }
-
-	// delete elements of a map
-	delete(colors, "red")
-
-	fmt.Println(colors)
+func printElements(c map[string]string) {
+	for i, color := range c {
+		fmt.Println("Description of the color", color, " key is ", i)
+	}
 }
