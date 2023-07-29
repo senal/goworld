59007182477c01d184fdd3ebb618c1efe1f09c78
Ranga Senal Fernando <ranga.fernando@gmail.com>
2023-07-29 15:32:18 +1000

    Manipulating maps

--- maps/main.go
+++ maps/main.go
@@ -3,10 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
-		"red":   "this is red",
-		"green": "this is green",
-	}
+	// ways to make an emtpy map
+	//1. var colors map[string]string
+	colors := make(map[string]string)
+
+	// how to append elements to an empty map
+	colors["red"] = "this is red"
+	colors["white"] = "this is white"
+
+	// colors := map[string]string{
+	// 	"red":   "this is red",
+	// 	"green": "this is green",
+	// }
+
+	// delete elements of a map
+	delete(colors, "red")
 
 	fmt.Println(colors)
 }
