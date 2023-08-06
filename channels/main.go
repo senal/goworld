d074caa0debc3c73fc21570a4d6b2547eab69357
Ranga Senal Fernando <ranga.fernando@gmail.com>
2023-08-06 22:32:59 +1000

    Repeating routines

--- channels/main.go
+++ channels/main.go
@@ -19,8 +19,8 @@ func main() {
 		go checkLink(link, c)
 	}
 
-	for i := 0; i < len(links); i++ {
-		fmt.Println(<-c)
+	for {
+		go checkLink(<-c, c)
 	}
 
 }
@@ -29,9 +29,9 @@ func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
-		c <- "Might be down!"
+		c <- link
 		return
 	}
 	fmt.Println(link, " is up!")
-	c <- "Yep, it's up!"
+	c <- link
 }
