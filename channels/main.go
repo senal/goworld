2d4f3cb66702b66a061187493637cfc9fa58644c
Ranga Senal Fernando <ranga.fernando@gmail.com>
2023-08-06 15:38:35 +1000

    Receiving messages from channel by main go routine from sub routine

--- channels/main.go
+++ channels/main.go
@@ -13,16 +13,25 @@ func main() {
 		"https://www.amazon.com",
 		"https://www.golang.com",
 	}
+	c := make(chan string, len(links))
 
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link, c)
 	}
+
+	for i := 0; i < len(links); i++ {
+		fmt.Println(<-c)
+	}
+
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
+		c <- "Might be down!"
+		return
 	}
 	fmt.Println(link, " is up!")
+	c <- "Yep, it's up!"
 }
