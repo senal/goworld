2b0c68a5483b6715efdb9341513706997f849cb9
Ranga Senal Fernando <ranga.fernando@gmail.com>
2023-08-06 14:32:36 +1000

    Printing site status

--- channels/main.go
+++ channels/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
 	links := []string{
 		"https://www.google.com",
@@ -9,4 +14,15 @@ func main() {
 		"https://www.golang.com",
 	}
 
+	for _, link := range links {
+		checkLink(link)
+	}
+}
+
+func checkLink(link string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down!")
+	}
+	fmt.Println(link, " is up!")
 }
