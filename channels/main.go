182f49a6f1d9da041e34a7487392b7cdc4a18948
Ranga Senal Fernando <ranga.fernando@gmail.com>
2023-08-06 14:25:35 +1000

    website status checker

---
+++ channels/main.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	links := []string{
+		"https://www.google.com",
+		"https://www.facebook.com",
+		"https://www.stackoverflow.com",
+		"https://www.amazon.com",
+		"https://www.golang.com",
+	}
+
+}
